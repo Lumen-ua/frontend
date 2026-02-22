aaef58300ebcc1a0f6de84e586aee3a58098f814
TR43Kolesovayeva <trkolesovayeva@gmail.com>
2026-02-21 21:25:14 -0500

    package added

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from "react";
 import { FiUser, FiMenu, FiEdit2, FiTarget, FiArrowLeft, FiAward, FiCheck, FiArrowRight, FiCamera } from 'react-icons/fi';
 
 import {
